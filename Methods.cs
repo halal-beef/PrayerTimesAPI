e127757d7e0b1d228b1e31912f59e7f3c12e84f0
smallpp420 <78730004+SmallPP420@users.noreply.github.com>
2022-05-09 20:15:09 +0100

    Add Different Methods & Add support for only getting the time for today

---
+++ Methods.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace API
+{
+    public class Methods
+    {
+        public enum CalcMethods
+        {
+            Hanafi_general,
+            Hanafi_Shafag_Abyad,
+            Shafi_Shafag_Ahmar,
+            Shia_Jafari
+        }
+    }
+}
--- PrayerTimeAPI.cs
+++ PrayerTimeAPI.cs
@@ -3,18 +3,33 @@
     using Newtonsoft.Json;
     using Newtonsoft.Json.Linq;
     using Windows.Devices.Geolocation;
+    using System.Linq;
     public class PrayerTimeAPI
     {
-        public List<PrayerTimeIdentification> GetPrayerTimes()
+        public List<PrayerTimeIdentification> GetPrayerTimes(Methods.CalcMethods CalcMethod)
         {
             List<PrayerTimeIdentification> prayerTimes = new();
-
             Geolocator locator = new();
             GeolocationAccessStatus status = Geolocator.RequestAccessAsync().GetAwaiter().GetResult();
-            if (status == GeolocationAccessStatus.Allowed) //Mashallah
+            if (status == GeolocationAccessStatus.Allowed)
             {
                 Geoposition pos = locator.GetGeopositionAsync().GetAwaiter().GetResult();
-                Uri allprayertimes = new Uri($@"https://www.moonsighting.com/time_json.php?year={DateTime.Now.Year}&tz=Europe/London&lat={pos.Coordinate.Latitude}&lon={pos.Coordinate.Longitude}&method=0&both=false&time=0");
+                Uri allprayertimes = new Uri($@"https://www.moonsighting.com/time_json.php?year={DateTime.Now.Year}&tz=Europe/London&lat={pos.Coordinate.Latitude}&lon={pos.Coordinate.Longitude}&method=0&both=0&time=0"); //default
+                switch (CalcMethod)
+                  {
+                    case Methods.CalcMethods.Hanafi_general:
+                        allprayertimes = new Uri($@"https://www.moonsighting.com/time_json.php?year={DateTime.Now.Year}&tz=Europe/London&lat={pos.Coordinate.Latitude}&lon={pos.Coordinate.Longitude}&method=0&both=0&time=0");
+                        break;
+                    case Methods.CalcMethods.Hanafi_Shafag_Abyad:
+                        allprayertimes = new Uri($@"https://www.moonsighting.com/time_json.php?year={DateTime.Now.Year}&tz=Europe/London&lat={pos.Coordinate.Latitude}&lon={pos.Coordinate.Longitude}&method=1&both=0&time=0");
+                        break;
+                    case Methods.CalcMethods.Shafi_Shafag_Ahmar:
+                        allprayertimes = new Uri($@"https://www.moonsighting.com/time_json.php?year={DateTime.Now.Year}&tz=Europe/London&lat={pos.Coordinate.Latitude}&lon={pos.Coordinate.Longitude}&method=2&both=0&time=0");
+                        break;
+                    case Methods.CalcMethods.Shia_Jafari:
+                        allprayertimes = new Uri($@"https://www.moonsighting.com/time_json.php?year={DateTime.Now.Year}&tz=Europe/London&lat={pos.Coordinate.Latitude}&lon={pos.Coordinate.Longitude}&method=3&both=0&time=0");
+                        break;
+                  }
                 var responseJSON = new HttpClient().GetStringAsync(allprayertimes).Result;
                 using (var sr = new StringReader(responseJSON))
                 using (var jr = new JsonTextReader(sr))
@@ -30,5 +45,12 @@
             }
             return null;
         }
+
+        public PrayerTimeIdentification GetPrayerTimesToday(Methods.CalcMethods CalcMethod)
+        {
+            PrayerTimeIdentification i = GetPrayerTimes(CalcMethod).Where(x => x.Day == DateTime.Now.ToString("MMM dd ddd")).First();
+            return i;        
+        }
+
     }
 }
